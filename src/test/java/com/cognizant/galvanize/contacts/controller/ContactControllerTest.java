bbc68033f1176dbb898f181ca02d62ae46287374
Mihir Parekh <mihir.m.parekh@gmail.com>
2021-06-12 15:36:57 -0400

    GET Contacts - By Surname and Givenname test cases working

--- src/test/java/com/cognizant/galvanize/contacts/controller/ContactControllerTest.java
+++ src/test/java/com/cognizant/galvanize/contacts/controller/ContactControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -57,7 +58,7 @@ public class ContactControllerTest {
         // Act &  // Assert
         RequestBuilder requestBuilder = get("/contacts");
         mockMvc.perform(requestBuilder).andExpect(status().isOk())
-        //        .andExpect(jsonPath("$[0]").doesNotExist())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
                 .andExpect(jsonPath("$.message", is("No data found")))
                 .andExpect(content().string("{\"message\":\"No data found\"}"));
     }
@@ -126,7 +127,7 @@ public class ContactControllerTest {
         when(mockContactRepository.findAll()).thenReturn(emptyContactBeanList);
 
         // Act &  // Assert
-        RequestBuilder requestBuilder = get("/contacts").requestAttr("givenName","some");
+        RequestBuilder requestBuilder = get("/contacts").requestAttr("givenName", "some");
         mockMvc.perform(requestBuilder).andExpect(status().isOk())
                 //        .andExpect(jsonPath("$[0]").doesNotExist())
                 .andExpect(jsonPath("$.message", is("No data found")))
@@ -149,7 +150,7 @@ public class ContactControllerTest {
         when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
 
         // Act &  // Assert
-        RequestBuilder requestBuilder = get("/contacts").param("givenName","Some")
+        RequestBuilder requestBuilder = get("/contacts").param("givenName", "Some")
                 .contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuilder).andExpect(status().isOk())
                 //        .andExpect(jsonPath("$[0]").doesNotExist())
@@ -172,7 +173,7 @@ public class ContactControllerTest {
         when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
 
         // Act &  // Assert
-        RequestBuilder requestBuilder = get("/contacts").param("givenName","John")
+        RequestBuilder requestBuilder = get("/contacts").param("givenName", "John")
                 .contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuilder).andExpect(status().isOk())
                 //        .andExpect(jsonPath("$[0]").doesNotExist())
@@ -199,16 +200,229 @@ public class ContactControllerTest {
         List<ContactBean> contactBeanList = new ArrayList<>();
         contactBeanList.add(contactBean);
         contactBeanList.add(contactBean1);
-        when(mockContactRepository.findAll()).thenReturn(contactBeanList);
+        when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
 
         // Act &  // Assert
-        RequestBuilder requestBuilder = get("/contacts").requestAttr("givenName","John");
+        RequestBuilder requestBuilder = get("/contacts").param("givenName", "John");
         mockMvc.perform(requestBuilder).andExpect(status().isOk())
                 //        .andExpect(jsonPath("$[0]").doesNotExist())
                 .andExpect(jsonPath("$.contactBeanResponseList[0].id", is("1")))
                 .andExpect(jsonPath("$.contactBeanResponseList[1].id", is("2")));
     }
 
+    // SURNAME TESTS
+
+    @Test
+    public void testIfSearchContactsBySurNameAndNoContactsFoundThenReturnResponseWithErrorMessage() throws Exception {
+        // ZERO CONDITION
+
+        // Arrange
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts").param("surName", "some");
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.message", is("No data found")))
+                .andExpect(content().string("{\"message\":\"No data found\"}"));
+    }
+
+    @Test
+    public void testIfSearchContactsByIncorrectSurNameAndNoContactsFoundThenReturnResponseWithErrorMessage() throws Exception {
+        // ZERO CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        contactBeanList.add(contactBean);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts").param("givenName", "Some")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.message", is("No data found")));
+    }
+
+    @Test
+    public void testIfSearchContactsBySurNameAndOneContactsFoundThenReturnResponseOneContact() throws Exception {
+        // ONE CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        contactBeanList.add(contactBean);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts").param("surName", "Doe")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.contactBeanResponseList[0].id", is("1")));
+    }
+
+    @Test
+    public void testIfSearchContactsBySurNameAndManyContactsFoundThenReturnResponseManyContact() throws Exception {
+        // MANY CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        ContactBean contactBean1 = new ContactBean();
+        contactBean1.setId("2");
+        contactBean1.setGivenName("Chris");
+        contactBean1.setSurName("Doe");
+        contactBean1.setPhoneNumber("222-2222-2222");
+
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        contactBeanList.add(contactBean);
+        contactBeanList.add(contactBean1);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts")
+                .param("surName", "Doe");
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.contactBeanResponseList[0].id", is("1")))
+                .andExpect(jsonPath("$.contactBeanResponseList[1].id", is("2")));
+    }
+
+
+    // SURNAME AND GIVEN NAME TESTS
+
+    @Test
+    public void testIfSearchContactsBySurNameAndGivenNameAndNoContactsFoundThenReturnResponseWithErrorMessage() throws Exception {
+        // ZERO CONDITION
+
+        // Arrange
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts")
+                .requestAttr("surName", "some").param("givenName", "other");
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.message", is("No data found")))
+                .andExpect(content().string("{\"message\":\"No data found\"}"));
+    }
+
+    @Test
+    public void testIfSearchContactsByIncorrectSurNameAndGivenNameAndNoContactsFoundThenReturnResponseWithErrorMessage() throws Exception {
+        // ZERO CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        contactBeanList.add(contactBean);
+        when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts")
+                .param("givenName", "Some")
+                .param("surName", "Other")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.message", is("No data found")));
+    }
+
+    @Test
+    public void testIfSearchContactsBySurNameAndGivenNameAndOneContactsFoundThenReturnResponseOneContact() throws Exception {
+        // ONE CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        List<ContactBean> contactBeanList = new ArrayList<>();
+        contactBeanList.add(contactBean);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(contactBeanList);
+        when(mockContactRepository.findByGivenName("John")).thenReturn(contactBeanList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts")
+                .param("givenName", "John")
+                .param("surName", "Doe")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.contactBeanResponseList[0].id", is("1")));
+    }
+
+    @Test
+    public void testIfSearchContactsBySurNameAndGivenNameAndManyContactsFoundThenReturnResponseManyContact() throws Exception {
+        // MANY CONDITION
+
+        // Arrange
+        ContactBean contactBean = new ContactBean();
+        contactBean.setId("1");
+        contactBean.setGivenName("John");
+        contactBean.setSurName("Doe");
+        contactBean.setPhoneNumber("111-1111-1111");
+
+        ContactBean contactBean1 = new ContactBean();
+        contactBean1.setId("2");
+        contactBean1.setGivenName("Chris");
+        contactBean1.setSurName("Doe");
+        contactBean1.setPhoneNumber("222-2222-2222");
+
+        ContactBean contactBean3 = new ContactBean();
+        contactBean3.setId("3");
+        contactBean3.setGivenName("John");
+        contactBean3.setSurName("Doe");
+        contactBean3.setPhoneNumber("333-3333-3333");
+
+        List<ContactBean> givenNameList = new ArrayList<ContactBean>();
+        givenNameList.add(contactBean);
+        givenNameList.add(contactBean3);
+
+        List<ContactBean> surNameList = new ArrayList<ContactBean>();
+        surNameList.add(contactBean);
+        surNameList.add(contactBean1);
+        surNameList.add(contactBean3);
+
+        when(mockContactRepository.findByGivenName("John")).thenReturn(givenNameList);
+        when(mockContactRepository.findBySurName("Doe")).thenReturn(surNameList);
+
+        // Act &  // Assert
+        RequestBuilder requestBuilder = get("/contacts")
+                .param("givenName", "John")
+                .param("surName", "Doe");
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                //        .andExpect(jsonPath("$[0]").doesNotExist())
+                .andExpect(jsonPath("$.contactBeanResponseList[0].id", is("1")))
+                .andExpect(jsonPath("$.contactBeanResponseList[1].id", is("3")));
+
+    }
 
 
 }
